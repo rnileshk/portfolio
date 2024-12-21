99aec56c28cf251753428dc201a9a2412364bcfa
NILESH KUMAR <140897234+rnileshk@users.noreply.github.com>
2024-12-21 20:27:42 +0530

    Update Socials.jsx

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import { FaEnvelope, FaFacebook, FaGithub, FaLinkedinIn, FaTwitter, FaWhatsapp } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path: ""},
-    {icon: <FaLinkedinIn />, path: ""},
-    {icon: <FaTwitter />, path: ""},
-    {icon: <FaEnvelope />, path: ""},
-    {icon: <FaWhatsapp />, path: ""},
-    {icon: <FaFacebook />, path: ""},
+    {icon: <FaGithub />, path: "https://github.com/rnileshk/"},
+    {icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/rnilesh/"},
+    {icon: <FaTwitter />, path: "https://x.com/rnilesh_dev"},
+    {icon: <FaEnvelope />, path: "https://mail.google.com/mail/u/0/?fs=1&to=imnileshkumar06@gmail.com&su=SUBJECT&body=BODY&tf=cm"},
+    {icon: <FaWhatsapp />, path: "https://wa.link/yqc1ip"},
+    {icon: <FaFacebook />, path: "https://www.facebook.com/profile.php?id=100069901102714&mibextid=ZbWKwL"},
 ];
 
 const Socials = ({ containerStyles, iconStyles }) => {
